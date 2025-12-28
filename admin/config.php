e487d64b17f7fda2ed1c9d77220f955f5f8c6101
Ahmad <chaarahmad0@gmail.com>
2025-12-28 12:50:22 +0200

    chore: initial OpenCart setup

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp2\htdocs\openCart/admin/');
+define('DIR_SYSTEM', 'C:\xampp2\htdocs\openCart/system/');
+define('DIR_DATABASE', 'C:\xampp2\htdocs\openCart/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp2\htdocs\openCart/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp2\htdocs\openCart/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp2\htdocs\openCart/system/config/');
+define('DIR_IMAGE', 'C:\xampp2\htdocs\openCart/image/');
+define('DIR_CACHE', 'C:\xampp2\htdocs\openCart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp2\htdocs\openCart/download/');
+define('DIR_LOGS', 'C:\xampp2\htdocs\openCart/system/logs/');
+define('DIR_CATALOG', 'C:\xampp2\htdocs\openCart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp2\htdocs\openCart/catalog/');
+define('DIR_SYSTEM', 'C:\xampp2\htdocs\openCart/system/');
+define('DIR_DATABASE', 'C:\xampp2\htdocs\openCart/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp2\htdocs\openCart/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp2\htdocs\openCart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp2\htdocs\openCart/system/config/');
+define('DIR_IMAGE', 'C:\xampp2\htdocs\openCart/image/');
+define('DIR_CACHE', 'C:\xampp2\htdocs\openCart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp2\htdocs\openCart/download/');
+define('DIR_LOGS', 'C:\xampp2\htdocs\openCart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
